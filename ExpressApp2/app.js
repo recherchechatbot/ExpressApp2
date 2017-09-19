73369aafc7a7e27d9d884b14a1ae8f86c0d575cc
a.rousselot <a.rousselot@DT-130301>
2017-09-19 17:31:27 +0200

    token

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -497,7 +497,7 @@ app.get('/', (req, res) => {
             facebookBot.doSubscribeRequest();
         }, 3000);
     } else {
-        res.send('Wrong verification token);
+        res.send('Wrong verification token');
     }
 });
 
