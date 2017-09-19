3d287ce12792483c8af59e7d86561e8eba234c8b
a.rousselot <a.rousselot@DT-130301>
2017-09-19 15:37:22 +0200

    oki

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -490,7 +490,7 @@ let facebookBot = new FacebookBot();
 app.use(bodyParser.text({ type: 'application/json' }));
 
 app.get('/', (req, res) => {
-    if (req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+    if (req.query['hub.verify_token'] === 'test') {
         res.send(req.query['hub.challenge']);
 
         setTimeout(() => {
