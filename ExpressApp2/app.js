51869051dfe1cc4eb18fb51a2908566952eea22a
a.rousselot <a.rousselot@DT-130301>
2017-09-28 17:15:53 +0200

    ok

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -155,6 +155,7 @@ app.post('/ai', (req, res) => {
         
     }
     else if (req.body.result.action === 'input.unknown') {
+        console.log(req.body.result.action);
 
         let messagedata = 'Je suis désolé mais je ne comprends pas encore votre requête. Souhaitez vous que je vous redirige vers un interlocuteur humain?';
 
