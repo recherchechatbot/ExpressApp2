4a19b901469d5af1932cc1fb4310e7956ef66365
a.rousselot <a.rousselot@DT-130301>
2017-09-29 10:25:51 +0200

    ok

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -5,7 +5,7 @@ const app = express();
 const request = require('request');
 var http = require("http");
 const apiaiApp = require('apiai')('30dfeddc13344176b6cefa6c09056e73');
-const PAGE_ACCESS_TOKEN = 'EAAMkZAtH8lc4BAH5prZA4p5IufCe5eJOGn7nVqiBCezLVUUkKKvlFIytG8dOKkQ7A4ZAnTzB5ZB3XhBMyfjplklbGL4TDNpLBfYfFTkRgbsYVmcp3iYKMUVhLwNXp2gAWfFDIKpf7fioqAJya9kkXSWXk3s006Wv0LWrRfVATQZDZD';
+const PAGE_ACCESS_TOKEN = 'EAACEdEose0cBACrTLG6HIkEHkZCNMBvfiSQPHgyFC0iyc1HQTsQM1SvaLPja0gXznCjv9IP9PFCkNdd2hPV3alUaUlY7TtYabULmreWWxCORNVPjihkEEi7N0WGZBt9zkOnDtGFH3ULfoGUqFxKSTNvxncsjxW454B9l1JVz6afEoixNUvnJkoB6vKZAJR6YZBVNqFbD2QZDZD';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
