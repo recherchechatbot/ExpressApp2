9b4da50a2078943cf61d586f40bb01edac291896
a.rousselot <a.rousselot@DT-130301>
2017-10-05 17:28:41 +0200

    gggg

--- ExpressApp2/stores/user_store.js
+++ ExpressApp2/stores/user_store.js
@@ -39,6 +39,7 @@ class UserStore extends Store {
         console.log("DEBUT getByFbId fbId = " + fbId);
         let currentUser = {};
         this.data.forEach((userData) => {
+            console.log("userData = " + JSON.stringify(userData));
             console.log("on compare " + userData.fbId + " avec " + fbId);
             if (userData.fbId === fbId) {
                 console.log("TROUVEEEEEEEEEEEEEEEEEEEEEEE");
