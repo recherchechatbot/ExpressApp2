b3bfea7665b9ed06d925ba3c9e02f01b1b5aeaa7
a.rousselot <a.rousselot@DT-130301>
2017-10-06 12:31:32 +0200

    persistent menu

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -614,11 +614,51 @@ class FacebookBot {
             setTimeout(() => resolve(), delay);
         });
     }
+    setupPersistentMenu(res) {
+    var messageData =
+        {
+            "persistent_menu": [
+                {
+                    "locale": "default",
+                    "composer_input_disabled": false,
+                    "call_to_actions": [
+                        {
+                            "title": "Se déconnecter",
+                            "type": "postback",
+                            "payload":"se deconnecter"
+                        }
+                    ]
+                }
+            ]
+        };
+    // Start the request
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=" + FB_PAGE_ACCESS_TOKEN,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        form: messageData
+    },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Print out the response body
+                res.send(body);
 
+            } else {
+                // TODO: Handle errors
+                res.send(body);
+            }
+        });
+
+}
 }
 
 
 let facebookBot = new FacebookBot();
+
+
+app.get('/setup', (req, res) => {
+    setupPersistentMenu(res);
+});
 
 
 
