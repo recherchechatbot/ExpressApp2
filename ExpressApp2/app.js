29da87e7b15da9d72efa8831154c8d434fce8209
a.rousselot <a.rousselot@DT-130301>
2017-10-06 09:29:06 +0200

    ok

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -11,7 +11,7 @@ const JSONbig = require('json-bigint');
 const async = require('async');
 const https = require('https');
 
-const REST_PORT =(process.env.PORT || 5000;
+const REST_PORT =(process.env.PORT || 5000);
 const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
 const APIAI_LANG = 'fr';
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
