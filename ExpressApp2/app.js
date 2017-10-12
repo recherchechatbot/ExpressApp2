6e10f866be1dd004a5a037d2b3564ca9d7b86fa7
a.rousselot <a.rousselot@DT-130301>
2017-10-12 15:34:22 +0200

    a

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -1049,7 +1049,7 @@ app.get('/recherche/recette/:m', (req, res) => {
                         "IdRecette": 70,
                         "Titre": "Soupe thaï au lait de coco et émincé de poulet",
                         "Description": "Peler et couper en quatre la carotte et l'oignon. Emincer en lamelle le poulet.Plonger dans une grande casserole le poulet émincé, l'oignon et la carotte. Ajouter le bouquet garni, saler et poivrer. Couvrir et laisser cuire 30 minutes à feu doux et à couvert.  Verser le bouillon de volaille dans une casserole. Ajouter la citronnelle,      le gingembre, la pâte de curry et le lait de coco. Mélanger et porter à ébullition.Ajouter les petits pois et les champignons coupés en quatre puis laisser frémir 5 minutes. Ajouter le poulet et laisser à nouveau 5 minutes à feu doux. Ajouter les tomates cerises coupées en deux, le jus de citron et la coriandre ciselée. Servir sans plus attendre.\n",
-                        "ImageUrl": "https://driveimg1.intermarche.com/fr/Ressources/images/publication/4916.jpg",
+                        "ImageUrl": "https://img15.hostingpics.net/pics/8935644916.png", /*"https://driveimg1.intermarche.com/fr/Ressources/images/publication/4916.jpg"*/
                         "Personnes": 4,
                         "Difficulte": 1,
                         "DureePreparation": 25,
@@ -1091,7 +1091,7 @@ app.get('/recherche/recette/:m', (req, res) => {
                         "IdRecette": 23,
                         "Titre": "Salade de billes de melon, tomates cerises et mozzarella",
                         "Description": "Détaillé le melon en petites billes, otez le pédoncule des tomates cerises et bien les laver.Dans un saladier, melangez les billes de melon, de mozzarella et les tomates cerises.Versez le vinaigre balsamique et l'huile d'olive puis le sel et le poivre.Melangez et rajoutez le basilic et les feuilles de menthe.Mettre au frais 1h avant de servir.",
-                        "ImageUrl": "https://driveimg1.intermarche.com/fr/Ressources/images/publication/4668.jpg",
+                        "ImageUrl": "https://img15.hostingpics.net/pics/7422914668.png",/*"https://driveimg1.intermarche.com/fr/Ressources/images/publication/4668.jpg"*/
                         "Personnes": 4,
                         "Difficulte": 1,
                         "DureePreparation": 10,
@@ -1118,7 +1118,7 @@ app.get('/recherche/recette/:m', (req, res) => {
                         "IdRecette": 39,
                         "Titre": "Clafoutis aux tomates cerises",
                         "Description": "Utilisez 1 cs d'huile d'olive pour huiler le plat à gratin, puis frottez le avec la gousse d'aïl. Préchauffez le four à 210°C (th 8). Lavez et séchez les tomates, puis passez les 5 minutes à la poêle dans l'huile restante, avec les herbes de Provence. Otez du feu.Hachez la mozzarella. Fouettez dans un saladier les oeufs, la crème, la mozzarella et la gousse écrasée. Ajoutez aux tomates dans la poele refroidie. Salez et poivrez, puis remuez doucement.\nVersez dans le plat à gratin, et enfournez pour un quart d'heure. Au moment de servir, parsemez de feuilles de basilic.\n",
-                        "ImageUrl": "https://driveimg1.intermarche.com/fr/Ressources/images/publication/4911.jpg",
+                        "ImageUrl": "https://img15.hostingpics.net/pics/8707974911.png",/*"https://driveimg1.intermarche.com/fr/Ressources/images/publication/4911.jpg"*/
                         "Personnes": 4,
                         "Difficulte": 1,
                         "DureePreparation": 10,
