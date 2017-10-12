015b9cb0b4a14b53c5ea4b8f82ce6be63092b580
a.rousselot <a.rousselot@DT-130301>
2017-10-12 09:36:25 +0200

    a

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -1584,9 +1584,9 @@ app.post('/ai', (req, res) => {
                     };
                     console.log('juste avant le dotextresponse');
                     facebookBot.sendFBSenderAction(sender, "typing_on")
-                        .then(() => facebookBot.sleep(1000))
                         .then(() => facebookBot.doTextResponse(sender_id, text))
                         .then(() => facebookBot.sleep(1000))
+                        .then(() => facebookBot.sendFBSenderAction(sender, "typing_on")
                         .then(() => facebookBot.sendFBMessage(sender_id, messagedata))
                     
 
