7e8ef98d86d7851553df15eefcdf6cad5d8aef25
a.rousselot <a.rousselot@DT-130301>
2017-10-16 17:07:58 +0200

    a

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -1758,12 +1758,82 @@ app.post('/ai', (req, res) => {
 
                     let text = "Resultats de votre recherche sur le point de vente de " + user_profile.namePdvFavori;
 
+                //    let messagedata = {
+                //        "attachment": {
+                //            "type": "template",
+                //            "payload": {
+                //                "template_type": "list",
+                //                "top_element_style": "compact",
+                //                "elements": [
+                //                    {
+                //                        "title": "Classic T-Shirt Collection",
+                //                        "subtitle": "See all our colors",
+                //                        "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                //                        "buttons": [
+                //                            {
+                //                                "title": "View",
+                //                                "type": "web_url",
+                //                                "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                //                                "messenger_extensions": true,
+                //                                "webview_height_ratio": "tall",
+                //                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                //                            }
+                //                        ]
+                //                    },
+                //                    {
+                //                        "title": "Classic White T-Shirt",
+                //                        "subtitle": "See all our colors",
+                //                        "default_action": {
+                //                            "type": "web_url",
+                //                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                //                            "messenger_extensions": true,
+                //                            "webview_height_ratio": "tall",
+                //                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                //                        }
+                //                    },
+                //                    {
+                //                        "title": "Classic Blue T-Shirt",
+                //                        "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                //                        "subtitle": "100% Cotton, 200% Comfortable",
+                //                        "default_action": {
+                //                            "type": "web_url",
+                //                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                //                            "messenger_extensions": true,
+                //                            "webview_height_ratio": "tall",
+                //                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                //                        },
+                //                        "buttons": [
+                //                            {
+                //                                "title": "Shop Now",
+                //                                "type": "web_url",
+                //                                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                //                                "messenger_extensions": true,
+                //                                "webview_height_ratio": "tall",
+                //                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                //                            }
+                //                        ]
+                //                    }
+                //                ],
+                //                "buttons": [
+                //                    {
+                //                        "title": "View More",
+                //                        "type": "postback",
+                //                        "payload": "payload"
+                //                    }
+                //                ]
+                //            }
+                //        }
+                //    }
+                //}
+                    
+
                     let messagedata = {
                         "attachment": {
                             "type": "template",
                             "payload": {
-                                "template_type": "generic",
-                                "image_aspect_ratio": "square",
+                                "template_type": "list",
+                                "top_element_style": "compact",
+                                //"image_aspect_ratio": "square",
                                 "elements": [
                                     {
                                         "title": r[0].Libelle,
